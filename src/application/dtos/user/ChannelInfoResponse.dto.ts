0b24e5cd0dd138d56b804c7e9196f5a03e7311ba
User <user@example.com>
2025-11-27 04:04:30 +0900

    Include stream key in my channel response

--- src/application/dtos/user/ChannelInfoResponse.dto.ts
+++ src/application/dtos/user/ChannelInfoResponse.dto.ts
@@ -4,12 +4,15 @@ import { UserResponseDto } from './UserResponse.dto';
 export class ChannelInfoResponseDto {
   user: UserResponseDto;
   stream: Stream | null;
+  streamKey?: string;
 
   constructor(props: {
     user: UserResponseDto;
     stream: Stream | null;
+    streamKey?: string;
   }) {
     this.user = props.user;
     this.stream = props.stream;
+    this.streamKey = props.streamKey;
   }
 }
